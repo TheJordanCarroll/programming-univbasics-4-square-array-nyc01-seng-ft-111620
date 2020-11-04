0c61a1a9b211d7221928576a789da95b6876cfac
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-04 01:47:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
-def square_array(array)
-  count = 0
-  new_array= []
-  while count < array.length do
-    new_array.push(array[count] ** 2)
-  end
-  new_array
-end
+#def square_array(array)
+  #count = 0
+  #new_array= []
+  #while count < array.length do
+   # new_array.push(array[count] ** 2)
+  #end
+ # new_array
+#end
